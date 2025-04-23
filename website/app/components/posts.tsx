3539eda879dfb9f4b847b33306930807cb9bc5d4
Adam Godel <agodel@bu.edu>
2025-04-23 09:45:01 -0400

    Made dark mode the only option for real

--- website/app/components/posts.tsx
+++ website/app/components/posts.tsx
@@ -22,10 +22,10 @@ export function BlogPosts() {
             href={`/notes/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[80px] tabular-nums">
+              <p className="text-neutral-400 w-[80px] tabular-nums">
                 {"Week "+post.metadata.week}
               </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              <p className="text-neutral-100 tracking-tight">
                 {post.metadata.title}
               </p>
             </div>
