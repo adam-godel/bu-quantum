f366571c4b8fc3664f3be8ae5121efe2e607ace6
Nico Jackson <nicolajack@gmail.com>
2025-08-14 10:27:45 -0400

    Update nav.tsx

--- website/app/components/nav.tsx
+++ website/app/components/nav.tsx
@@ -5,8 +5,14 @@ const navItems = {
   '/': {
     name: 'home',
   },
+  '/about': {
+    name: 'about',
+  },
   '/notes': {
     name: 'notes',
+  },
+  '/contact': {
+    name: 'contact',
   },
 }
 
@@ -15,10 +21,10 @@ export function Navbar() {
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-center relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex flex-row items-center justify-between relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
+          <div className="flex">
             <Image 
               src="/logo.png"
               alt="BU Quantum"
@@ -26,12 +32,15 @@ export function Navbar() {
               height={24}
               className="flex align-middle relative px-2"
             />
+          </div>
+          <div className="flex flex-row space-x-0">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="text-xl transition-all hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  // blur hover effect still too subtle, will come back to it later
+                  className="text-xl transition-all hover:text-neutral-200 hover:drop-shadow-[0_0_8px_rgba(239,68,68,0.8)] flex align-middle relative py-1 px-2 m-1"
                 >
                   {name}
                 </Link>
