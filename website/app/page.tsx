c2066cdf1f521d81147b991ba8e3219863ac33fb
Adam Godel <agodel@bu.edu>
2025-08-15 13:42:49 -0400

    Added brick and flicker effect

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -38,23 +38,24 @@ export default function Page() {
         </div>
         
         {/* logo and tagline */}
-        <div className="lg:w-1/2 flex flex-col items-center lg:items-end mt-16 lg:mt-0">
-          {/*<div className="text-center">
-            <img
-              src="/logo.png"
-              alt="BU Quantum"
-              width={400}
-              height={400}
-              className="mb-6 hover:scale-105 transition-transform duration-300"
-            />
-          </div>*/}
-          <div className="[box-shadow:0_0.1vw_0.4vw_#fff7f7,0_0.4vw_0.6vw_#e97272,0_0_4vw_0.4vw_#cc0000,inset_0_0_1.5vw_0.4vw_#cc0000,inset_0_0_0.4vw_0.2vw_#e97272,inset_0_0_0.5vw_0.2vw_#fff7f7] rounded-3xl px-12 pt-12 pb-6" style={{ overflow: "visible" }}>
+        <div className="relative lg:w-1/2 flex flex-col items-center lg:items-end mt-16 lg:mt-0 p-8">
+          <div
+            className="absolute -inset-16 bg-cover bg-center opacity-30 -z-10"
+            style={{ backgroundImage: `url('/brick.jpg')` }}
+          />
+          <div
+            className="absolute -inset-16 pointer-events-none -z-10"
+            style={{
+              background: 'radial-gradient(circle, rgba(0,0,0,0) 30%, rgba(0,0,0,0.4) 60%, rgba(0,0,0,0.9) 90%)',
+            }}
+          />
+          <div className="group [box-shadow:0_0.1vw_0.4vw_#fff7f7,0_0.4vw_0.6vw_#e97272,0_0_4vw_0.4vw_#cc0000,inset_0_0_1.5vw_0.4vw_#cc0000,inset_0_0_0.4vw_0.2vw_#e97272,inset_0_0_0.5vw_0.2vw_#fff7f7] rounded-3xl px-12 pt-12 pb-6 hover:animate-[flicker_9.66s_linear_infinite]">
               <img
                 src="/logo.png"
                 alt="BU Quantum"
                 width={400}
                 height={400}
-                className="mb-6 hover:scale-105 transition-transform duration-300"
+                className="mb-6 group-hover:animate-[flicker_3.51s_linear_infinite]"
               />
           </div>
         </div>
