6cc3e59357e8402e3a9f25aa7c2f27da0ba8677d
Adam Godel <agodel@bu.edu>
2025-08-24 12:14:16 -0400

    Typo fix

--- website/app/about/page.tsx
+++ website/app/about/page.tsx
@@ -22,7 +22,7 @@ export default function About() {
                 <div className="grid grid-cols-1 md:grid-cols-3 justify-center">
                     <MemberProfile name="adam godel" role="director" imageUrl="/adam.png" linkedin="https://www.linkedin.com/in/adam-godel" bio={<>passionate about quantum computing.<br/>qsl fellow @ wiser/nnl.<br/>awards @ iquhack & qrise.<br/>not fault-tolerant myself.</>}/>
                     <MemberProfile name="nico jackson" role="vice director" imageUrl="/nico.png" linkedin="https://www.linkedin.com/in/nicola-jackson-64b22833b" bio={<>passionate about web dev and swe.<br/>big fan of gta v and bears.<br/>"6’2".</>}/>
-                    <MemberProfile name="travis meyer" role="head of finance" imageUrl="/travis.png" linkedin="https://www.linkedin.com/in/travis-meyer-2b313a299" bio={<>passionate about something.<br/>swe & excel victim.<br/>5'8.</>}/>
+                    <MemberProfile name="travis meyer" role="head of finance" imageUrl="/travis.png" linkedin="https://www.linkedin.com/in/travis-meyer-2b313a299" bio={<>passionate about something.<br/>swe & excel victim.<br/>5’8.</>}/>
                     <div className="flex-col md:flex-row md:col-span-3 flex justify-center gap-8">
                         <MemberProfile name="jacky lin" role="head of operations" imageUrl="/jacky.png" linkedin="https://www.linkedin.com/in/lin-jacky" bio={<>passionate about boba.<br/>badminton/tv enthusiast.<br/>always exploring new domains.<br/>i love birds.</>}/>
                         <MemberProfile name="timothy wright" role="head of pedagogy" imageUrl="/timothy.png" linkedin="https://www.linkedin.com/in/timmyjr11" bio={<>passionate about robots.<br/>lead swe and start-up founder.<br/>quantum shill.<br/>existing is chill.</>}/>
