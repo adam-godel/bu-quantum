79d0568e6a062adcfab0b9df833208928b25a5ad
Adam Godel <agodel@bu.edu>
2025-10-08 18:10:10 -0400

    Published week 5

--- website/app/components/nav.tsx
+++ website/app/components/nav.tsx
@@ -17,7 +17,7 @@ const navItems = {
   },
 }
 
-const scrollingText = "week 5: oct 8, 2025 @ 6pm in cas 208"
+const scrollingText = "week 6: oct 22, 2025 @ 6pm in cas 208"
 
 export function Navbar() {
   return (
