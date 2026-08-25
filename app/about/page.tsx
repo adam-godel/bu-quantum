66c7262f3e77750d767764f15b91e2d21fab01e7
Adam Godel <agodel@bu.edu>
2026-08-24 22:23:27 -0400

    Minor tweak

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -56,7 +56,7 @@ export default function About() {
                     <MemberProfile name="Yebin Song" imageUrl="/yebin.jpg" website="https://www.linkedin.com/in/yebin-song-11158232a" bio="Data Science @ BU '28."/>
                     <MemberProfile name="Allen Chen" imageUrl="/allen.jpg" website="https://www.linkedin.com/in/allenchen28" bio="Electrical Engineering @ BU '28."/>
                     <MemberProfile name="Artem Arefev" imageUrl="/artem.jpg" website="https://www.linkedin.com/in/artem-arefev-0789563a5" bio="Mathematics & Physics @ BU '29."/>
-                    <MemberProfile name="Rahil Shah" imageUrl="/rahil.jpg" website="https://github.com/rahilrshah" bio="Mathematics and Physics @ BU '27."/>
+                    <MemberProfile name="Rahil Shah" imageUrl="/rahil.jpg" website="https://github.com/rahilrshah" bio="Mathematics & Physics @ BU '27."/>
                     <MemberProfile name="Alex Poremba" imageUrl="/alex.jpg" website="https://scc1.bu.edu/poremba/" bio="Professor @ BU CS & Physics."/>
                 </div>
             </section>
